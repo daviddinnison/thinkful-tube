037dc9e6d190279e15eeb0b3041e3d9cc2bd1bc2
daviddinnison <david.g.dinnison@gmail.com>
2017-06-21 12:44:54 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// 						STEP ONE --- design state
+
+//url to search
+//create template variable
+
+
+// 						STEP TWO --- state mod functions
+
+//get data from API (getDataFromApi) 
+
+
+//						STEP THREE --- render
+
+//creates array of results (displayYoutubeSearchData)
+//render results into html via template
+
+
+
+//						STEP FOUR ---- event listening
+
+//listen for event submit ('.search-form')
+//find and store search query (variables to design later)
+//call API data request (getDataFromApi) and has display data function (displayYoutubeSearchData) ready for rendering
+
+
+
+
+//doc ready - stores event listen
